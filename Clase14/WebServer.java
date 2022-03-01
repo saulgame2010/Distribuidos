6f540b6b8c4cd642fdc13976b0c6a8a6c3414f20
saulgame2010 <saulgame2010@gmail.com>
2022-03-01 10:29:46 -0600

    Se acabó el ejercicio de la clase14 falta comentar el código

--- Clase14/WebServer.java
+++ Clase14/WebServer.java
@@ -32,11 +32,13 @@ import java.io.OutputStream;
 import java.math.BigInteger;
 import java.net.InetSocketAddress;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.Executors;
 
 public class WebServer {
     private static final String TASK_ENDPOINT = "/task";
     private static final String STATUS_ENDPOINT = "/status";
+    private static final String SEARCH_ENDPOINT = "/search";
 
     private final int port;
     private HttpServer server;
@@ -67,9 +69,11 @@ public class WebServer {
 
         HttpContext statusContext = server.createContext(STATUS_ENDPOINT);
         HttpContext taskContext = server.createContext(TASK_ENDPOINT);
+        HttpContext searchContext = server.createContext(SEARCH_ENDPOINT);
 
         statusContext.setHandler(this::handleStatusCheckRequest);
         taskContext.setHandler(this::handleTaskRequest);
+        searchContext.setHandler(this::handleStatusSearchRequest);
 
         server.setExecutor(Executors.newFixedThreadPool(8));
         server.start();
@@ -130,6 +134,58 @@ public class WebServer {
 
         String responseMessage = "El servidor está vivo\n";
         sendResponse(responseMessage.getBytes(), exchange);
+    }
+
+    private void handleStatusSearchRequest(HttpExchange exchange) throws IOException {
+        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) {
+            exchange.close();
+            return;
+        }
+        Headers headers = exchange.getRequestHeaders();
+        boolean isDebugMode = false;
+        if (headers.containsKey("X-Debug") && headers.get("X-Debug").get(0).equalsIgnoreCase("true")) {
+            isDebugMode = true;
+        }
+
+        long startTime = System.nanoTime();
+
+        byte[] requestBytes = exchange.getRequestBody().readAllBytes();
+        byte[] responseBytes = countString(requestBytes);
+
+        long finishTime = System.nanoTime();
+
+        if (isDebugMode) {
+            String debugMessage = String.format("La operación tomó %d nanosegundos", finishTime - startTime);
+            exchange.getResponseHeaders().put("X-Debug-Info", Arrays.asList(debugMessage));
+        }
+        sendResponse(responseBytes, exchange);
+    }
+
+    private byte[] countString(byte[] requestBytes) {
+        String bodyString = new String(requestBytes);
+        String [] params = bodyString.split(",");
+        int n = Integer.parseInt(params[0]);
+        int index = 0;
+        int apariciones = 0;
+        int asciiInicio = 65; // Este es el código ASCII de la 'A'
+        int asciiFinal = 90; // Este es el código ASCII de la 'Z'
+        Random random = new Random();
+        StringBuilder cadenota = new StringBuilder(n*4);        
+        for (int i = 0; i < n; i++) {            
+            cadenota.append((char) (asciiInicio + (int) (random.nextFloat() * (asciiFinal - asciiInicio + 1))));
+            cadenota.append((char) (asciiInicio + (int) (random.nextFloat() * (asciiFinal - asciiInicio + 1))));
+            cadenota.append((char) (asciiInicio + (int) (random.nextFloat() * (asciiFinal - asciiInicio + 1))));
+            //El 32 es el espacio en el código ASCII
+            cadenota.append((char)32);
+        }
+        // Esta es la primer aparición de la cadena IPN por eso la variable apariciones = 1 en este punto        
+        index = cadenota.indexOf(params[1]);                               
+        while(index >= 0) {
+            // System.out.println(index);            
+            index = cadenota.indexOf("IPN", index + 1);
+            apariciones++;
+        }        
+        return String.format("El numero de apariciones de tu cadena es %s\n", apariciones).getBytes();
     }
 
     private void sendResponse(byte[] responseBytes, HttpExchange exchange) throws IOException {
