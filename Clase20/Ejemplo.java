5447efeaa93a8911460cf9fe4c23123c3046c675
saulgame2010 <saulgame2010@gmail.com>
2022-03-22 16:49:09 -0600

    Se hicieron los hilos, hay que probarlos

--- Clase20/Ejemplo.java
+++ Clase20/Ejemplo.java
@@ -6,15 +6,195 @@ import java.io.IOException;
 public class Ejemplo {
 
     public static void main(String[] args) throws IOException {
+        long inicio, fin;
 
-        Hilo fi_micro1 = new Hilo("34.125.12.105:80/search");
-        Hilo fi_micro2 = new Hilo("34.125.12.105:80/search");
-        Hilo fi_micro3 = new Hilo("34.125.12.105:80/search");
-        Hilo fi_micro4 = new Hilo("34.125.12.105:80/search");
+        Hilo f1_micro1 = new Hilo("34.125.12.105:80/search");
+        Hilo f1_micro2 = new Hilo("34.125.12.105:80/search");
+        Hilo f1_micro3 = new Hilo("34.125.12.105:80/search");
+        Hilo f1_micro4 = new Hilo("34.125.12.105:80/search");
 
-        fi_micro1.start();
-        fi_micro2.start();
-        fi_micro3.start();
-        fi_micro4.start();
+        inicio = System.nanoTime();
+        f1_micro1.start();
+        f1_micro2.start();
+        f1_micro3.start();
+        f1_micro4.start();
+        try {
+            f1_micro1.join();
+            f1_micro2.join();
+            f1_micro3.join();
+            f1_micro4.join();
+            fin = System.nanoTime();
+            System.out.println("Las 4 peticiones a f1-micro tomaron " + (fin - inicio) + " nano segundos en ejecutarse");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Hilo n1_standard1_1 = new Hilo("34.125.223.242:80/search");
+        Hilo n1_standard1_2 = new Hilo("34.125.223.242:80/search");
+        Hilo n1_standard1_3 = new Hilo("34.125.223.242:80/search");
+        Hilo n1_standard1_4 = new Hilo("34.125.223.242:80/search");
+
+        inicio = System.nanoTime();
+        n1_standard1_1.start();
+        n1_standard1_2.start();
+        n1_standard1_3.start();
+        n1_standard1_4.start();
+        try {
+            n1_standard1_1.join();
+            n1_standard1_2.join();
+            n1_standard1_3.join();
+            n1_standard1_4.join();
+            fin = System.nanoTime();
+            System.out.println("Las 4 peticiones a n1-standard-1 tomaron " + (fin - inicio) + " nano segundos en ejecutarse");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Hilo n1_standard2_1 = new Hilo("34.125.96.56:80/search");
+        Hilo n1_standard2_2 = new Hilo("34.125.96.56:80/search");
+        Hilo n1_standard2_3 = new Hilo("34.125.96.56:80/search");
+        Hilo n1_standard2_4 = new Hilo("34.125.96.56:80/search");
+
+        inicio = System.nanoTime();
+        n1_standard2_1.start();
+        n1_standard2_2.start();
+        n1_standard2_3.start();
+        n1_standard2_4.start();
+        try {
+            n1_standard2_1.join();
+            n1_standard2_2.join();
+            n1_standard2_3.join();
+            n1_standard2_4.join();
+            fin = System.nanoTime();
+            System.out.println("Las 4 peticiones a n1-standard-2 tomaron " + (fin - inicio) + " nano segundos en ejecutarse");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Hilo n1_standard4_1 = new Hilo("34.125.179.82:80/search");
+        Hilo n1_standard4_2 = new Hilo("34.125.179.82:80/search");
+        Hilo n1_standard4_3 = new Hilo("34.125.179.82:80/search");
+        Hilo n1_standard4_4 = new Hilo("34.125.179.82:80/search");
+
+        inicio = System.nanoTime();
+        n1_standard4_1.start();
+        n1_standard4_2.start();
+        n1_standard4_3.start();
+        n1_standard4_4.start();
+        try {
+            n1_standard4_1.join();
+            n1_standard4_2.join();
+            n1_standard4_3.join();
+            n1_standard4_4.join();
+            fin = System.nanoTime();
+            System.out.println("Las 4 peticiones a n1-standard-4 tomaron " + (fin - inicio) + " nano segundos en ejecutarse");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Hilo n1_highcpu2_1 = new Hilo("34.125.48.210:80/search");
+        Hilo n1_highcpu2_2 = new Hilo("34.125.48.210:80/search");
+        Hilo n1_highcpu2_3 = new Hilo("34.125.48.210:80/search");
+        Hilo n1_highcpu2_4 = new Hilo("34.125.48.210:80/search");
+
+        inicio = System.nanoTime();
+        n1_highcpu2_1.start();
+        n1_highcpu2_2.start();
+        n1_highcpu2_3.start();
+        n1_highcpu2_4.start();
+        try {
+            n1_highcpu2_1.join();
+            n1_highcpu2_2.join();
+            n1_highcpu2_3.join();
+            n1_highcpu2_4.join();
+            fin = System.nanoTime();
+            System.out.println("Las 4 peticiones a n1-highcpu-2 tomaron " + (fin - inicio) + " nano segundos en ejecutarse");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Hilo n1_highcpu4_1 = new Hilo("34.125.164.47:80/search");
+        Hilo n1_highcpu4_2 = new Hilo("34.125.164.47:80/search");
+        Hilo n1_highcpu4_3 = new Hilo("34.125.164.47:80/search");
+        Hilo n1_highcpu4_4 = new Hilo("34.125.164.47:80/search");
+
+        inicio = System.nanoTime();
+        n1_highcpu4_1.start();
+        n1_highcpu4_2.start();
+        n1_highcpu4_3.start();
+        n1_highcpu4_4.start();
+        try {
+            n1_highcpu4_1.join();
+            n1_highcpu4_2.join();
+            n1_highcpu4_3.join();
+            n1_highcpu4_4.join();
+            fin = System.nanoTime();
+            System.out.println("Las 4 peticiones a n1-highcpu-4 tomaron " + (fin - inicio) + " nano segundos en ejecutarse");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Hilo e2_micro_1 = new Hilo("34.125.84.203:80/search");
+        Hilo e2_micro_2 = new Hilo("34.125.84.203:80/search");
+        Hilo e2_micro_3 = new Hilo("34.125.84.203:80/search");
+        Hilo e2_micro_4 = new Hilo("34.125.84.203:80/search");
+
+        inicio = System.nanoTime();
+        e2_micro_1.start();
+        e2_micro_2.start();
+        e2_micro_3.start();
+        e2_micro_4.start();
+        try {
+            e2_micro_1.join();
+            e2_micro_2.join();
+            e2_micro_3.join();
+            e2_micro_4.join();
+            fin = System.nanoTime();
+            System.out.println("Las 4 peticiones a e2-micro tomaron " + (fin - inicio) + " nano segundos en ejecutarse");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Hilo e2_highcpu2_1 = new Hilo("34.125.19.92:80/search");
+        Hilo e2_highcpu2_2 = new Hilo("34.125.19.92:80/search");
+        Hilo e2_highcpu2_3 = new Hilo("34.125.19.92:80/search");
+        Hilo e2_highcpu2_4 = new Hilo("34.125.19.92:80/search");
+
+        inicio = System.nanoTime();
+        e2_highcpu2_1.start();
+        e2_highcpu2_2.start();
+        e2_highcpu2_3.start();
+        e2_highcpu2_4.start();
+        try {
+            e2_highcpu2_1.join();
+            e2_highcpu2_2.join();
+            e2_highcpu2_3.join();
+            e2_highcpu2_4.join();
+            fin = System.nanoTime();
+            System.out.println("Las 4 peticiones a e2-highcpu-2 tomaron " + (fin - inicio) + " nano segundos en ejecutarse");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Hilo e2_highcpu4_1 = new Hilo("34.106.248.131:80/search");
+        Hilo e2_highcpu4_2 = new Hilo("34.106.248.131:80/search");
+        Hilo e2_highcpu4_3 = new Hilo("34.106.248.131:80/search");
+        Hilo e2_highcpu4_4 = new Hilo("34.106.248.131:80/search");
+
+        inicio = System.nanoTime();
+        e2_highcpu4_1.start();
+        e2_highcpu4_2.start();
+        e2_highcpu4_3.start();
+        e2_highcpu4_4.start();
+        try {
+            e2_highcpu4_1.join();
+            e2_highcpu4_2.join();
+            e2_highcpu4_3.join();
+            e2_highcpu4_4.join();
+            fin = System.nanoTime();
+            System.out.println("Las 4 peticiones a e2-highcpu-4 tomaron " + (fin - inicio) + " nano segundos en ejecutarse");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
